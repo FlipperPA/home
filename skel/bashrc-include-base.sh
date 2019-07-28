e886e187db5c778d882a4a985c3a43cda229c1a3
Allen, Timothy <tallen@wharton.upenn.edu>
2019-07-28 07:44:44 -0400

    Move base functions to included file.

---
+++ skel/bashrc-include-base.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Source global definitions
+if [ -f /etc/bashrc ]; then
+    . /etc/bashrc
+fi
+
+# Share the current directory on port 8000
+alias sharedir="python -m http.server 8000"
+
+# Useful aliases
+alias ccat='pygmentize -O style=monokai,linenos=1 -f terminal -g'
+
+# Get keys from central dev server, save local authorized keys and append
+alias get_keys='cd ~ && cp .ssh/authorized_keys .ssh/authorized_keys.orig && scp -r tallen@wrds-pub1-dev:~/.{ssh,aws} . && git remote set-url origin git@github.com:FlipperPA/home.git && cat .ssh/authorized_keys.orig >> .ssh/authorized_keys'
