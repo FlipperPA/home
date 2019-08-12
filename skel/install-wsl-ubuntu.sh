7e5373853e116e3229c87372a1ea731ab9030e4d
Allen, Timothy <tallen@wharton.upenn.edu>
2019-08-12 09:06:48 -0400

    Add git and bash completion.

--- skel/install-wsl-ubuntu.sh
+++ skel/install-wsl-ubuntu.sh
@@ -1,6 +1,7 @@
+mkdir -p .ssh
 touch .ssh/authorized_keys
 sudo apt update
-sudo apt install python3-pip python3-venv
+sudo apt install python3-pip python3-venv git bash-completion
 sudo umount /mnt/c
 sudo mount -t drvfs C: /mnt/c -o metadata
 mkdir /mnt/c/projects
