1f8ddf8d9663238f09c14d798c9ce1c07ffc4aeb
Allen, Timothy <tallen@wharton.upenn.edu>
2019-08-12 09:08:34 -0400

    Re-add -y.

--- skel/install-wsl-ubuntu.sh
+++ skel/install-wsl-ubuntu.sh
@@ -1,7 +1,7 @@
 mkdir -p .ssh
 touch .ssh/authorized_keys
 sudo apt update
-sudo apt install python3-pip python3-venv git bash-completion
+sudo apt -y install python3-pip python3-venv git bash-completion
 sudo umount /mnt/c
 sudo mount -t drvfs C: /mnt/c -o metadata
 mkdir -p /mnt/c/projects
