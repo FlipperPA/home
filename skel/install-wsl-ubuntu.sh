06d9851d235ecc1f9b2a7946ba31cd5398500f0d
Timothy Allen <tallen@wharton.upenn.edu>
2019-08-12 08:44:51 -0400

    Update install-wsl-ubuntu.sh

--- skel/install-wsl-ubuntu.sh
+++ skel/install-wsl-ubuntu.sh
@@ -1,6 +1,6 @@
 touch .ssh/authorized_keys
 sudo apt update
-sudo apt install python3-pip python3-venv
+sudo apt -y install python3-pip python3-venv
 sudo umount /mnt/c
 sudo mount -t drvfs C: /mnt/c -o metadata
 mkdir /mnt/c/projects
