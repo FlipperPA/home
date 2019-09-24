1de0aa995e40b1696f846b60af15c536c001eef8
Allen, Timothy <tallen@wharton.upenn.edu>
2019-09-24 10:06:28 -0700

    Install SQL Server drivers on Mac.

--- skel/install-mac.sh
+++ skel/install-mac.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # Install Home Brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install freetds memcached postgresql
+brew tap microsoft/mssql-release https://github.com/Microsoft/homebrew-mssql-release
+brew update
+brew install freetds memcached postgresql msodbcsql17
