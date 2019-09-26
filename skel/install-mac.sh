20d391e828c908e6a32edb0f03ed425645cf1c74
Allen, Timothy <tallen@wharton.upenn.edu>
2019-09-26 10:08:50 -0700

    Update bashrc for Mac.

--- skel/install-mac.sh
+++ skel/install-mac.sh
@@ -4,4 +4,3 @@
 brew tap microsoft/mssql-release https://github.com/Microsoft/homebrew-mssql-release
 brew update
 brew install freetds memcached libmemcached postgresql msodbcsql17
-export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
