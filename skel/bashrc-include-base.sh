24613091661b1b5ea4d3a131b9e65772215ed7b5
Allen, Timothy <tallen@wharton.upenn.edu>
2019-10-18 10:57:03 -0400

    Line endings.

--- skel/bashrc-include-base.sh
+++ skel/bashrc-include-base.sh
@@ -10,5 +10,5 @@ alias sharedir="python -m http.server 8000"
 # Useful aliases
 alias ccat='pygmentize -O style=monokai,linenos=1 -f terminal -g'
 
-# Get keys from central dev server, save local authorized keys and append
+# Get keys, save local authorized keys and append
 alias get_keys='cd ~ && cp .ssh/authorized_keys .ssh/authorized_keys.orig && scp -r tallen@wrds-pub1-dev.wharton.upenn.edu:~/.{ssh,aws} . && git remote set-url origin git@github.com:FlipperPA/home.git && cat .ssh/authorized_keys.orig >> .ssh/authorized_keys'
