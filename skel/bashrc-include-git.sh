ceac70d6f96317cb18bd98f3e579e7b24193adf3
Allen, Timothy <tallen@wharton.upenn.edu>
2020-01-11 20:25:44 -0500

    Update gitmerged_delete to be more explicit.

--- skel/bashrc-include-git.sh
+++ skel/bashrc-include-git.sh
@@ -1,5 +1,14 @@
 # git aliases
+func_gitmerged_delete() {
+    for branch in $(git branch -r --merged master | grep origin | grep -v develop | grep -v stage | grep -v master)
+    do
+        echo "*** Branch ${branch#*/} is fully merged to master, deleting locally and remote. ***"
+        git branch -d "${branch#*/}"
+        git push origin --delete "${branch#*/}"
+    done
+}
+
 alias gitohshit='git reflog && echo "To revert: git reset HEAD@{index}"'
 alias gitmerged='git branch --merged | egrep -v "(^\*|master|develop|stage)"'
-alias gitmerged_delete='git branch --merged | egrep -v "(^\*|master|develop|stage)" | xargs git branch -d && git branch -r --merged | grep -v "(^\*|master|develop|stage)" | sed "s/origin\///" | xargs -n 1 git push --delete origin'
+alias gitmerged_delete=func_gitmerged_delete
 alias gitprunemerged="git branch -r --merged | grep -v 'master\|develop' | sed 's/origin\///' | xargs -n 1 git push --delete origin && git remote prune origin"
