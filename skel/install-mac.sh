72c7bd4e158abc9bfe3fb0cbbfabb9f2525518e5
Allen, Timothy <tallen@wharton.upenn.edu>
2020-03-07 18:03:01 -0500

    Add git tab completion.

--- skel/install-mac.sh
+++ skel/install-mac.sh
@@ -3,4 +3,4 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew tap microsoft/mssql-release https://github.com/Microsoft/homebrew-mssql-release
 brew update
-brew install freetds memcached libmemcached postgresql msodbcsql17
+brew install freetds memcached libmemcached postgresql msodbcsql17 git bash-completion
