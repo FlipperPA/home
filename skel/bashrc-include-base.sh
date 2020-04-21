e46a19f0905a7c504a3b2cf21617c6e2077e643a
Allen, Timothy <tallen@wharton.upenn.edu>
2020-04-21 11:30:52 -0400

    Add copy code alias.

--- skel/bashrc-include-base.sh
+++ skel/bashrc-include-base.sh
@@ -9,6 +9,7 @@ alias sharedir="python -m http.server 8000"
 
 # Useful aliases
 alias ccat='pygmentize -O style=monokai,linenos=1 -f terminal -g'
+alias copycode='pygmentize -O style=monokai -f rtf -g'
 
 # Get keys, save local authorized keys and append
 alias get_keys='cd ~ && cp .ssh/authorized_keys .ssh/authorized_keys.orig && scp -r tallen@wrds-pub1-dev.wharton.upenn.edu:~/.{ssh,aws} . && git remote set-url origin git@github.com:FlipperPA/home.git && cat .ssh/authorized_keys.orig >> .ssh/authorized_keys'
