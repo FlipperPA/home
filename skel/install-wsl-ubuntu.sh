8f69ff7449285650f73141ab2c0a024382b00900
Allen, Timothy <tallen@wharton.upenn.edu>
2020-06-14 16:23:00 -0400

    Update to Ubuntu 20 and PostgreSQL 12.

--- skel/install-wsl-ubuntu.sh
+++ skel/install-wsl-ubuntu.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
-# Sets up the Windows Subsystem for Linux version of Ubuntu 18.04 for Django development
+# Sets up the Windows Subsystem for Linux version of Ubuntu 20.04 for Django development
 
 # Install NodeJS and NPM repository
-curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 
-# Temporary fix for pasting into IPython
+# Temporary fix for pasting into IPython with Windows Terminal
 mkdir -p ~/.ipython/profile_default/
 cp ~/skel/ipython_config.py ~/.ipython/profile_default/
 
-# Install PostgreSQL, Python packages for venv creation
+# Install Python and other essential packages
 sudo apt update
-sudo apt -y install python3-pip python3-venv git bash-completion postgresql-client-10 postgresql-10 postgresql-server-dev-10 ntpdate tdsodbc
+sudo apt -y install python3-pip python3-venv git bash-completion tdsodbc
+
+# Install PostgreSQL 12
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" |sudo tee  /etc/apt/sources.list.d/pgdg.list
+sudo apt -y install postgresql-12 postgresql-client-12 postgresql-server-dev-12
 
 # Let user sudo without passwords.
 sudo chmod 644 /etc/sudoers
@@ -18,9 +23,9 @@ sudo sed -i 's/%sudo\tALL=(ALL:ALL) ALL/%sudo\tALL=(ALL:ALL) NOPASSWD: ALL/' /et
 sudo chmod 440 /etc/sudoers
 
 # Allow connections from Windows host OS to WSL-2 Ubuntu PostgreSQL
-echo "listen_addresses = '*'" >> /etc/postgresql/10/main/postgresql.conf
-echo "# Allow connecting to postgresql from outside WSL-2 Ubuntu" >> /etc/postgresql/10/main/pg_hba.conf
-echo "host	all	all	all	trust" >> /etc/postgresql/10/main/pg_hba.conf
+echo "listen_addresses = '*'" >> /etc/postgresql/12/main/postgresql.conf
+echo "# Allow connecting to postgresql from outside WSL-2 Ubuntu" >> /etc/postgresql/12/main/pg_hba.conf
+echo "host	all	all	all	trust" >> /etc/postgresql/12/main/pg_hba.conf
 
 # Start and Enable PostgreSQL
 sudo service postgresql start
