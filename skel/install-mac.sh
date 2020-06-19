d5679f41e18cbeeece4ba216428a1719bf54fcbe
Timothy Allen <flipper@peregrinesalon.com>
2020-06-19 10:34:43 -0400

    Ruby has been replaced with bash. Awesome!

--- skel/install-mac.sh
+++ skel/install-mac.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Install Home Brew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 brew tap microsoft/mssql-release https://github.com/Microsoft/homebrew-mssql-release
 brew update
 brew install freetds memcached libmemcached postgresql msodbcsql17 git bash-completion
