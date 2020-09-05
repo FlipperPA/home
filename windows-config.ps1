2c1b0b283fa88bd8895098113214168529a31b3d
Timothy Allen <flipper@peregrinesalon.com>
2020-09-04 23:01:54 -0400

    Use small taskbar icons.

--- windows-config.ps1
+++ windows-config.ps1
@@ -2,7 +2,9 @@
 # It should be run in Administrator mode from PowerShell.
 
 # Set the Windows Taskbar to never combine items (Windows 7 style)
-Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name TaskbarGlomLevel -Value 1
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name TaskbarGlomLevel -Value 2
+# Set the Windows Taskbar to use small icons
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name TaskbarSmallIcons -Value 1
 
 # Finally, stop explorer; it will auto-restart
 Get-Process -Name explorer | Stop-Process
