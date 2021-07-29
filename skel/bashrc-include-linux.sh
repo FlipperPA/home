bfc33b3a3c4e587d15517e45b2bf0916292d8553
Allen, Timothy <tallen@wharton.upenn.edu>
2021-07-29 11:34:51 -0400

    Add token run command.

--- skel/bashrc-include-linux.sh
+++ skel/bashrc-include-linux.sh
@@ -7,5 +7,8 @@ if [ -f /etc/bash_completion.d/git ]; then
     source /etc/bash_completion.d/git
 fi
 
+# include my tokens
+. ~/.tokens/set.sh
+
 # Check the window size after each command and update the values of LINES and COLUMNS.
 shopt -s checkwinsize
