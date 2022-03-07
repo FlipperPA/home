0bec869f87d9d5256cdfb8f9131ffd87b5406183
Allen, Timothy <tallen@wharton.upenn.edu>
2022-03-07 16:49:02 -0500

    Simplify emacs init file.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,32 +1,18 @@
 ;; init.el --- Emacs configuration
 
-;; INSTALL PACKAGES
-;; --------------------------------------
-(require 'package)
-
-(add-to-list 'package-archives
-       '("melpa" . "http://melpa.org/packages/") t)
-
-(package-initialize)
-(when (not package-archive-contents)
-  (package-refresh-contents))
-
-(defvar myPackages
-  '(better-defaults
-    elpy
-    material-theme))
-
-(mapc #'(lambda (package)
-    (unless (package-installed-p package)
-      (package-install package)))
-      myPackages)
-
 ;; BASIC CUSTOMIZATION
 ;; --------------------------------------
 
 (setq inhibit-startup-message t) ;; hide the startup message
-(load-theme 'material t) ;; load material theme
 (global-linum-mode t) ;; enable line numbers globally
+(setq column-number-mode t) ;; enable column numbers in status bar
+
+;; Adhere to Python black's 88 character limit per line
+(require 'whitespace)
+(setq whitespace-line-column 89) ;; 88 + 1
+(setq whitespace-style '(face lines-tail))
+
+(add-hook 'prog-mode-hook 'whitespace-mode)
 
 ;; enable visual feedback on selections
 (setq transient-mark-mode t)
@@ -45,31 +31,6 @@
 
 ;; Always use spaces.
 (setq-default indent-tab-mode nil)
-
-;; open up CFM / CFC files in HTML mode
-(add-to-list 'auto-mode-alist '("\\.cfm\\'" . html-mode))
-(add-to-list 'auto-mode-alist '("\\.cfc\\'" . html-mode))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (material-theme elpy better-defaults))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
-;; Activate elpy for Python formatting
-(elpy-enable)
-
-;; Enable Flycheck
-(when (require 'flycheck nil t)
-  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
-  (add-hook 'elpy-mode-hook 'flycheck-mode))
 
 ;; Don't litter up everything with backup files
 (setq make-backup-files nil)
