a3985abf4d3147ff5124302121ed831ff18831fa
Allen, Timothy <tallen@wharton.upenn.edu>
2022-03-07 17:14:53 -0500

    Add .pypirc to get_keys.

--- skel/bashrc-include-base.sh
+++ skel/bashrc-include-base.sh
@@ -15,7 +15,7 @@ alias ccat='pygmentize -O style=monokai,linenos=1 -f terminal -g'
 alias copycode='pygmentize -O style=monokai -f rtf -g'
 
 # Get keys, save local authorized keys and append
-alias get_keys='cd ~ && cp .ssh/authorized_keys .ssh/authorized_keys.orig && scp -r tallen@wrds-pub1-dev.wharton.upenn.edu:~/.{ssh,aws,tokens} . && git remote set-url origin git@github.com:FlipperPA/home.git && cat .ssh/authorized_keys.orig >> .ssh/authorized_keys'
+alias get_keys='cd ~ && cp .ssh/authorized_keys .ssh/authorized_keys.orig && scp -r tallen@wrds-pub1-dev.wharton.upenn.edu:~/.{ssh,aws,tokens,pypirc} . && git remote set-url origin git@github.com:FlipperPA/home.git && cat .ssh/authorized_keys.orig >> .ssh/authorized_keys'
 
 # Set up my bash prompt. See: https://scriptim.github.io/bash-prompt-generator/
 PS1='\[\e[0m\]\t \[\e[0m\](\[\e[0;92m\]$(git branch 2>/dev/null | grep '"'"'^*'"'"' | colrm 1 2)\[\e[0m\]) \[\e[0m\]\w \[\e[0m\]\u\[\e[0m\]@\[\e[0;38;5;127m\]\h \[\e[0m\](\[\e[0m\]!\[\e[0m\]\!\[\e[0m\])\n\[\e[0;96m\]\$ \[\e[0m\]'
