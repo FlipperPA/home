d450732435676914061191e46c6acc60c2a4b933
Allen, Timothy <tallen@wharton.upenn.edu>
2024-12-18 15:45:12 -0500

    Remove faulty node config.

--- skel/install-wsl-ubuntu-msodbc.sh
+++
@@ -1,4 +0,0 @@
-curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-curl https://packages.microsoft.com/config/ubuntu/19.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
-apt-get update
-ACCEPT_EULA=Y apt-get install msodbcsql17 mssql-tools unixodbc-dev
